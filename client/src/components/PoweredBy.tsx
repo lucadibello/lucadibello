421ce421317a97e557e73b649cc08c8dd9e70765
luca.dibello@student.supsi.ch <info@lucadibello.ch>
2021-08-10 16:31:12 +0200

    Created "Powered By" section component

---
+++ client/src/components/PoweredBy.tsx
@@ -0,0 +1,50 @@
+import { Box, Typography } from '@material-ui/core'
+import { classes } from 'istanbul-lib-coverage';
+import React from 'react'
+import DockerLogo from './DockerLogo';
+import ReactLogo from './ReactLogo';
+
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    poweredByContainer: {
+      display: "inline-flex",
+      textAlign: "center",
+      alignItems: "center"
+    },
+    poweredLogo: {
+      marginRight: "10px",
+      marginLeft: "10px"
+    }
+  })
+)
+
+export default function PoweredBy () {
+  const classes = useStyles()
+  return (
+    <React.Fragment>
+      <Box className={classes.poweredByContainer}>
+        <Typography>
+          Powered by 
+        </Typography>
+
+        <Box className={classes.poweredLogo}>
+          <ReactLogo />
+        </Box>
+
+        <Typography>
+          +
+        </Typography>
+        
+        <Box className={classes.poweredLogo}>
+          <DockerLogo />
+        </Box>
+        
+        <Typography>
+          with ❤️
+        </Typography>
+      </Box>
+    </React.Fragment>
+  );
+}
