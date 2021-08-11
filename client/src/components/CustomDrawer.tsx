a7054d504b314aa95bc0f6db665cd636c2b06076
luca.dibello@student.supsi.ch <info@lucadibello.ch>
2021-08-11 11:27:40 +0200

    Added waves + resized items to fill available space

--- client/src/components/CustomDrawer.tsx
+++ client/src/components/CustomDrawer.tsx
@@ -7,12 +7,21 @@ import {
   ListItemIcon,
   ListItemText,
   SvgIcon,
+  Button,
+  Typography,
+  Box
 } from '@material-ui/core';
 
 import {
   MailOutlineOutlined as MailIcon,
+  EmojiPeople as PeopleIcon,
+  Work as WorkIcon,
+  AccountTree as ProjectIcon,
+  Close as CloseIcon,
   SvgIconComponent
 } from '@material-ui/icons';
+
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 
 interface LucaDrawerProps {
   open: boolean,
@@ -28,35 +37,74 @@ interface DrawerItem {
 const listItems: DrawerItem[] = [
   {
     text: "Bio",
-    icon: MailIcon
+    icon: PeopleIcon
   },
   {
     text: "WorkFlow",
-    icon: MailIcon
+    icon: ProjectIcon
   },
   {
     text: "Progetti",
-    icon: MailIcon
+    icon: WorkIcon
   },
   {
-    text: "Servizi",
+    text: "Servizi offerti",
     icon: MailIcon
   },
 ]
 
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    drawerListItems: {
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center !important',
+      justifyContent: 'center'
+    },
+    drawerHeader: {
+      display: 'flex',
+      alignItems: 'center',
+      padding: theme.spacing(0, 1),
+      // necessary for content to be below app bar
+      ...theme.mixins.toolbar,
+      justifyContent: 'center',
+      background: '#161819',
+      color: 'white'
+    },
+    listItem: {
+      height: "10vh"
+    },
+    drawerSpacer: {
+      flexGrow: 1
+    }
+  })
+)
+
 export default function CustomDrawer (props: LucaDrawerProps) {
+  const classes = useStyles()
   return (
-    <nav aria-label="mailbox folders">
+    <nav aria-label="swipable menu">
       <SwipeableDrawer
         anchor={"left"}
         open={props.open}
         onClose={props.onClose}
         onOpen={props.onOpen}
         disableBackdropTransition={false}
+        className={classes.drawerListItems}
       >
+        <div className={classes.drawerHeader}>
+          <Typography variant="h5" component="h2">
+            Navigazione
+          </Typography>
+        </div>
+        <Box>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+            <path fill="#161819" fillOpacity="1" d="M0,160L205.7,160L411.4,288L617.1,192L822.9,64L1028.6,160L1234.3,160L1440,288L1440,0L1234.3,0L1028.6,0L822.9,0L617.1,0L411.4,0L205.7,0L0,0Z"></path>
+          </svg>
+        </Box>
         <List>
           {listItems.map((item) => (
-            <ListItem button key={item.text}>
+            <ListItem button key={item.text} className={classes.listItem}>
               <ListItemIcon>
                 <SvgIcon component={item.icon} />
               </ListItemIcon>
@@ -64,6 +112,14 @@ export default function CustomDrawer (props: LucaDrawerProps) {
             </ListItem>
           ))}
         </List>
+        <div className={classes.drawerSpacer} />
+        <Button
+          color="secondary"
+          onClick={props.onClose}
+          startIcon={<CloseIcon />}
+        >
+          Chiudi
+        </Button>
       </SwipeableDrawer>
     </nav>
   )
