7946316f6599130a7cebea90a12a18cbcb0339af
Luca Di Bello <info@lucadibello.ch>
2022-07-23 20:30:59 +0200

    New index page with navbar + spinning model

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,23 @@
 import { Box, Heading } from "@chakra-ui/react"
+import { OrbitControls, Stage } from "@react-three/drei"
+import { Canvas } from "@react-three/fiber"
+import { Suspense } from "react"
+import FloatingModel from "../components/FloatingModel"
+import Navbar from "../components/Navbar"
 
 const Homepage = () => {
   return (
     <Box>
-      <Heading>My new website will look great!</Heading>
+      <Box w="full">
+        <Navbar />
+      </Box>
+
+      {/* ThreeJS animation */}
+      <Box w={"full"} h="500px">
+        <Canvas dpr={[1, 2]} camera={{ fov: 50, position: [0, 0, 5] }}>
+          <FloatingModel />
+        </Canvas>
+      </Box>
     </Box>
   )
 }
