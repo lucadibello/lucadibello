4bb9cad8003a02e2391ed47ef8272d8eb86a6d44
Luca Di Bello <info@lucadibello.ch>
2022-07-24 12:10:54 +0200

    Added loading

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,16 @@
 import { Box, Heading } from "@chakra-ui/react"
-import { OrbitControls, Stage } from "@react-three/drei"
+import { Html, OrbitControls, Stage, useProgress } from "@react-three/drei"
 import { Canvas } from "@react-three/fiber"
 import { Suspense } from "react"
 import FloatingModel from "../components/FloatingModel"
+import Floor from "../components/Floor"
+import Lights from "../components/Lights"
 import Navbar from "../components/Navbar"
+
+const Loader = () => {
+  const { progress } = useProgress()
+  return <Html center>{progress}% loaded</Html>
+}
 
 const Homepage = () => {
   return (
@@ -14,8 +21,15 @@ const Homepage = () => {
 
       {/* ThreeJS animation */}
       <Box w={"full"} h="500px">
-        <Canvas dpr={[1, 2]} camera={{ fov: 50, position: [0, 0, 5] }}>
-          <FloatingModel />
+        <Canvas
+          camera={{ fov: 60, position: [-5, 2, 10] }}
+          shadows
+        >
+          <Lights />
+          <Suspense fallback={<Loader />}>
+            <FloatingModel />
+            <Floor />
+          </Suspense>
         </Canvas>
       </Box>
     </Box>
