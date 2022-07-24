e4a825fbb9b7db16e7b749f2ad3243f67a58f3c3
Luca Di Bello <info@lucadibello.ch>
2022-07-24 12:11:05 +0200

    Updated floating model component

--- components/FloatingModel.tsx
+++ components/FloatingModel.tsx
@@ -1,33 +1,27 @@
 import { Stage, OrbitControls, useGLTF } from "@react-three/drei"
-import { Suspense } from "react"
 
 const FloatingModel = () => {
   const { scene } = useGLTF("/isometric_bedroom/scene.gltf")
 
   return (
-    <Suspense>
-      <Stage intensity={1} shadowBias={-0.0015}> 
-        <ambientLight intensity={0.4} />
-        <directionalLight
-          castShadow
-          position={[-8, 16, -8]}
-          intensity={0}
-          shadow-mapSize-width={2048}
-          shadow-mapSize-height={2048}
-          shadow-camera-far={50}
-          shadow-camera-left={-10}
-          shadow-camera-right={10}
-          shadow-camera-top={10}
-          shadow-camera-bottom={-10}
-        />
-        <pointLight position={[0, 50, 0]} />
-        <mesh>
-          <primitive object={scene} /> 
-        </mesh>
+    <>
+      <Stage castShadow position={[10,10,0]}> 
+        <primitive object={scene} />
       </Stage>
-      <OrbitControls autoRotate enableZoom={true} enablePan={false} />
-    </Suspense>
+      <OrbitControls
+        autoRotate
+        enableZoom={true}
+        enablePan={false}
+        enableDamping={true}
+        dampingFactor={0.1}
+        rotateSpeed={0.6}
+        minZoom={1}
+        maxZoom={1}
+      />
+    </>
   )
 }
+
+useGLTF.preload("/isometric_bedroom/scene.gltf")
 
 export default FloatingModel
