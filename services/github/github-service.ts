46d1d6f13a394efb3831d9a6a6a9ac7020bcece1
Luca Di Bello <info@lucadibello.ch>
2023-05-30 23:00:37 +0200

    Now using local file as fallback

--- services/github/github-service.ts
+++ services/github/github-service.ts
@@ -4,8 +4,17 @@ export async function getRepos(): Promise<GithubRepository[]> {
   if (!process.env.NEXT_PUBLIC_GITHUB_API) {
     throw new Error("Missing GITHUB_API env variable")
   }
-
-  const response = await fetch(process.env.NEXT_PUBLIC_GITHUB_API)
-  const data = await response.json()
-  return data
+  
+  try {
+    const response = await fetch(process.env.NEXT_PUBLIC_GITHUB_API)
+    const data = await response.json()
+    return data
+  } catch (error) {
+    console.warn("Error fetching repos from Github API, using local storage", error)
+    // Return data from local file
+    return fetch("/data/github.json").then(r => r.json()).catch(err => {
+      console.warn("Error while fetching repos from local storage:", err)
+      return []
+    })
+  }
 }
