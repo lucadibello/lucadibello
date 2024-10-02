71132c9d5e2897afd0ed238ae07dcbec36ca58a5
Luca Di Bello <lucadibello@proton.me>
2024-10-02 11:01:23 +0200

    formatted + updated texts

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,25 +14,26 @@ import Script from "next/script";
 
 export default function Home() {
   // States
-  const [repos, setRepos] = useState<GithubRepository[]>([])
-  const [isLoadingRepos, setIsLoadingRepos] = useState<boolean>(true)
+  const [repos, setRepos] = useState<GithubRepository[]>([]);
+  const [isLoadingRepos, setIsLoadingRepos] = useState<boolean>(true);
 
   // Get current year
-  const currentYear = new Date().getFullYear()
+  const currentYear = new Date().getFullYear();
 
   // Get repos
   useEffect(() => {
-    setIsLoadingRepos(true)
+    setIsLoadingRepos(true);
     getRepos()
       .then((repos) => {
-        setRepos(repos)
+        setRepos(repos);
       })
       .catch((err) => {
-        console.warn(err)
-      }).finally(() => {
-        setIsLoadingRepos(false)
+        console.warn(err);
       })
-  }, [])
+      .finally(() => {
+        setIsLoadingRepos(false);
+      });
+  }, []);
 
   return (
     <>
@@ -41,11 +42,33 @@ export default function Home() {
         <meta name="description" content="Luca Di Bello personal website" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
-        <link rel="apple-touch-icon" sizes="192x192" href="/favicons/maskable/logo192.png" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
-        <link rel="mask-icon" href="/favicons/apple/safari-pinned-tab.svg" color="#5bbad5" />
+        <link
+          rel="apple-touch-icon"
+          sizes="192x192"
+          href="/favicons/maskable/logo192.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicons/apple/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicons/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicons/favicon-16x16.png"
+        />
+        <link
+          rel="mask-icon"
+          href="/favicons/apple/safari-pinned-tab.svg"
+          color="#5bbad5"
+        />
         <meta name="msapplication-TileColor" content="#cce3de" />
         <meta name="theme-color" content="#ffffff" />
 
@@ -101,7 +124,6 @@ export default function Home() {
         </script>
       </Head>
       <main>
-
         {/* Google Analytics */}
         <Script
           src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}`}
@@ -116,7 +138,6 @@ export default function Home() {
           gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}');
         `}
         </Script>
-
 
         {/* Pass IP to Header component */}
         <Header />
@@ -136,18 +157,28 @@ export default function Home() {
 
         {/* second section - who am i*/}
         <Box as="section" py="10" px="5" backgroundColor={palette.viridian}>
-          <Heading as="h2" size="lg" my={7} color={'white'}>
+          <Heading as="h2" size="lg" my={7} color={"white"}>
             About me
           </Heading>
           <Stack direction={"column"} spacing={5} mb={5}>
             <Text color={"white"} fontSize={"lg"} fontWeight={"bold"}>
-              I&apos;m a <Code bgColor={palette.mint_cream}>Software Engineer</Code> currently enrolled in the <Code bgColor={palette.mint_cream}>Master of Science in Informatics</Code> program at USI. <br />
+              I&apos;m a{" "}
+              <Code bgColor={palette.mint_cream}>Software Engineer</Code>
+              currently enrolled in the{" "}
+              <Code bgColor={palette.mint_cream}>
+                Master of Science in Informatics
+              </Code>
+              program at USI. <br />
             </Text>
             <Text color={"white"} fontSize={"lg"} fontWeight={"bold"}>
-              From a young age, I have had a deep passion for the world of computer science. Throughout my journey, I have continuously pursued self-improvement and gained valuable hands-on experience.
+              Since an early age, I’ve been deeply passionate about computer
+              science. Over the years, I’ve continually sought self-improvement
+              and gained valuable hands-on experience along the way.
             </Text>
             <Text color={"white"} fontSize={"lg"} fontWeight={"bold"}>
-              I invite you to <Code bgColor={palette.mint_cream}>explore my portfolio</Code> and discover the projects I have worked on.
+              Feel free to{" "}
+              <Code bgColor={palette.mint_cream}>explore my portfolio</Code>
+              and check out the projects I’ve worked on.
             </Text>
           </Stack>
         </Box>
@@ -167,10 +198,11 @@ export default function Home() {
         {/* third section - my projects */}
         <Box as="section" py={10} px="5" backgroundColor={palette.mint_cream}>
           <Heading as="h2" size="lg" mb="3">
-            My projects 🚀
+            Projects I've Worked On 🚀
           </Heading>
           <Text size={"lg"} fontWeight="bold" mb={2}>
-            I have worked on many projects, both personal and third party. Here are some of my most famous ones.
+            I’ve contributed to a variety of projects, both personal and
+            collaborative. Here are a few of the most notable ones.
           </Text>
 
           {/* Stack with repos */}
@@ -178,8 +210,17 @@ export default function Home() {
             <RepositoryList repositories={repos || []} />
 
             {/* view more on github  */}
-            <Link href={process.env.NEXT_PUBLIC_GITHUB_URL!} target="_blank" rel="noopener noreferrer">
-              <Button leftIcon={<FiGithub />} colorScheme='black' variant="outline" mt={5}>
+            <Link
+              href={process.env.NEXT_PUBLIC_GITHUB_URL!}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Button
+                leftIcon={<FiGithub />}
+                colorScheme="black"
+                variant="outline"
+                mt={5}
+              >
                 View more on Github 🚀
               </Button>
             </Link>
@@ -199,44 +240,61 @@ export default function Home() {
 
         {/* fourth section - contact me */}
         <Box as="section" py="10" px="5" backgroundColor={palette.viridian}>
-          <Heading as="h2" size="lg" my={7} color={'white'}>
+          <Heading as="h2" size="lg" my={7} color={"white"}>
             Contact me
           </Heading>
           <Text color={"white"} fontSize={"lg"} fontWeight={"bold"}>
-            I&apos;m always <Code bgColor={palette.mint_cream}>open to new opportunities</Code> and collaborations. Reach out to me if you want to get in touch!
+            I&apos;m always{" "}
+            <Code bgColor={palette.mint_cream}>open to new opportunities</Code>{" "}
+            and collaborations. Reach out to me if you want to get in touch!
           </Text>
 
           {/* Social buttons */}
           <Stack direction={{ base: "column", md: "row" }} spacing={5} mt={5}>
-            <Link href={process.env.NEXT_PUBLIC_GITHUB_URL!} target="_blank" rel="noopener noreferrer" _hover={{ textDecoration: "none" }}>
+            <Link
+              href={process.env.NEXT_PUBLIC_GITHUB_URL!}
+              target="_blank"
+              rel="noopener noreferrer"
+              _hover={{ textDecoration: "none" }}
+            >
               <Button
                 leftIcon={<FiGithub />}
-                colorScheme='black'
+                colorScheme="black"
                 _hover={{
-                  backgroundColor: "blackAlpha.200"
+                  backgroundColor: "blackAlpha.200",
                 }}
               >
                 Github
               </Button>
             </Link>
 
-            <Link href={process.env.NEXT_PUBLIC_LINKEDIN_URL!} target="_blank" rel="noopener noreferrer" _hover={{ textDecoration: "none" }}>
+            <Link
+              href={process.env.NEXT_PUBLIC_LINKEDIN_URL!}
+              target="_blank"
+              rel="noopener noreferrer"
+              _hover={{ textDecoration: "none" }}
+            >
               <Button
                 leftIcon={<FiLinkedin />}
-                colorScheme='black'
+                colorScheme="black"
                 _hover={{
-                  backgroundColor: "blackAlpha.200"
+                  backgroundColor: "blackAlpha.200",
                 }}
               >
                 LinkedIn
               </Button>
             </Link>
-            <Link href={"mailto:" + process.env.NEXT_PUBLIC_MAIL_ADDRESS!} target="_blank" rel="noopener noreferrer" _hover={{ textDecoration: "none" }}>
+            <Link
+              href={"mailto:" + process.env.NEXT_PUBLIC_MAIL_ADDRESS!}
+              target="_blank"
+              rel="noopener noreferrer"
+              _hover={{ textDecoration: "none" }}
+            >
               <Button
                 leftIcon={<FiMail />}
-                colorScheme='black'
+                colorScheme="black"
                 _hover={{
-                  backgroundColor: "blackAlpha.200"
+                  backgroundColor: "blackAlpha.200",
                 }}
               >
                 Email
@@ -256,7 +314,14 @@ export default function Home() {
         </Box>
 
         {/* footer */}
-        <Box as="footer" pt="10" pb={"100px"} px="5" backgroundColor={palette.mint_cream} textAlign={"center"}>
+        <Box
+          as="footer"
+          pt="10"
+          pb={"100px"}
+          px="5"
+          backgroundColor={palette.mint_cream}
+          textAlign={"center"}
+        >
           <Stack direction={"column"} mb={5}>
             <Heading as="h2" size="md">
               © Copyright {currentYear}, Luca Di Bello
@@ -268,5 +333,5 @@ export default function Home() {
         </Box>
       </main>
     </>
-  )
+  );
 }
